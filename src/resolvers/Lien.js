766375ac06da3aff68f311b9b49d75fa2090c1d4
Robbie Hughes <rwhughes91@gmail.com>
2020-04-17 11:41:16 -0700

    standardized county field on Lien

--- src/resolvers/Lien.js
+++ src/resolvers/Lien.js
@@ -1,17 +1,20 @@
+const toTitleCase = (str) => {
+  if (typeof str !== 'string') {
+    return str;
+  }
+  str = str.toLowerCase().split(' ');
+  let newStr = [];
+  for (let word of str) {
+    newStr.push(word.charAt(0).toUpperCase() + word.slice(1));
+  }
+  return newStr.join(' ');
+};
+
 const Lien = {
-  address(parent, args, context, info) {
-    const toTitleCase = (str) => {
-      if (typeof str !== 'string') {
-        return str;
-      }
-      str = str.toLowerCase().split(' ');
-      let newStr = [];
-      for (let word of str) {
-        newStr.push(word.charAt(0).toUpperCase() + word.slice(1));
-      }
-      return newStr.join(' ');
-    };
-    const { address } = parent;
+  county: ({ county }, args, context, info) => {
+    return toTitleCase(county);
+  },
+  address: ({ address }, args, context, info) => {
     return toTitleCase(address);
   },
 };
